7ab28d56a3cfe6cb5d3f61e53812a12a0fd6a853
1000 <fauxan1000@gmail.com>
2024-05-16 08:59:54 +0700

    loop

---
+++ php/loopin/coba.php
@@ -0,0 +1,14 @@
+<?php  
+$animals = array("cat", "dog", "snake", "ant", "lion"); 
+
+foreach ($animals as $animal) {
+  echo "$animal <br>";
+}
+?>  
+<?php
+$age = array("Rezky"=>"25", "Abduh"=>"29", "Iqbal"=>"33");
+
+foreach($age as $x => $val) {
+  echo "$x = $val<br>";
+}
+?>
